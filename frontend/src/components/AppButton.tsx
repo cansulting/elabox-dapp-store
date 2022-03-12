23172cc7b628f178a953c8752b3181604ba75e5a
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-03-12 09:50:20 +0000

    add button and snaptshot test

--- frontend/src/components/AppButton.tsx
+++ frontend/src/components/AppButton.tsx
@@ -1,43 +1,77 @@
-import React from 'react';
-import './appbutton.css';
-import { Badge, Image, Button }  from 'antd-mobile';
+import React from 'react'
 
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label?: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-  children?:any;
+import { Button, Spinner } from 'reactstrap'
+
+export interface AppButtonProps {
+    children?: string
+    color?:
+        | 'primary'
+        | 'secondary'
+        | 'success'
+        | 'danger'
+        | 'warning'
+        | 'info'
+        | 'light'
+        | 'dark'
+    outline: boolean
+    size: 'sm' | 'lg'
+    block: boolean
+    active: boolean
+    close: boolean
+    disabled: boolean
+    isProcessing: boolean
 }
+export const AppButton = (props: AppButtonProps): JSX.Element => {
+    if (props.isProcessing) {
+        return (
+            <Button {...props}>
+                <Spinner children="" />
+            </Button>
+        )
+    } else {
+        return (
+            <Button {...props} hi>
+                {props.children}
+            </Button>
+        )
+    }
+}
+// export interface ButtonProps {
+//   /**
+//    * Is this the principal call to action on the page?
+//    */
+//   primary?: boolean;
+//   /**
+//    * What background color to use
+//    */
+//   backgroundColor?: string;
+//   /**
+//    * How large should the button be?
+//    */
+//   size?: 'small' | 'medium' | 'large';
+//   /**
+//    * Button contents
+//    */
+//   label?: string;
+//   /**
+//    * Optional click handler
+//    */
+//   onClick?: () => void;
+//   children?:any;
+// }
 
-/**
- * Primary UI component for user interaction
- */
-export const AppButton = ({
-  ...props
-}: ButtonProps) => {
-  
-  return (
-    <Badge content="sd">
-      <button className='box-g' onClick={_ => {}}>
-        <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqHlvqVHOU13mP3tHH94KoX4cXTKbos0M8g&usqp=CAU' />
-      </button>
-    </Badge>  
-  );
-};
+// /**
+//  * Primary UI component for user interaction
+//  */
+// export const AppButton = ({
+//   ...props
+// }: ButtonProps) => {
+
+//   return (
+//     <Badge content="sd">
+//       <button className='box-g' onClick={_ => {}}>
+//         <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqHlvqVHOU13mP3tHH94KoX4cXTKbos0M8g&usqp=CAU' />
+//       </button>
+//     </Badge>
+//   );
+// };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
-// import { Grid, Button } from "antd-mobile";
 
-export class Home extends React.Component<{}, {}> {
-    constructor(parameters: any) {
-        super(parameters);
-    }
-    override render(): React.ReactNode {
-        return <div>sdfsdf</div>;
-        // return <Grid columns={4}>
-        //     <Grid.Item>
-        //         <Button>DFDFGDF</Button>
-        //     </Grid.Item>
-        // </Grid>;
-    }
+// export class Home extends React.Component<{}, {}> {
+//     constructor(parameters: any) {
+//         super(parameters);
+//     }
+//     override render(): React.ReactNode {
+//         return <div>sdfsdf</div>;
+//     }
+// }
+export const Home = ():JSX.Element=>{
+    return <div>aoisdjoaisjd</div>
 }
