5e7cd636d99cb12e948824adca51c0ad01f7333a
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-03-16 11:25:29 +0000

    added front end test

---
+++ frontend_test/dist/App.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare const App: (props: any) => JSX.Element;
+export default App;
---
+++ frontend_test/src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Home } from "dapp-store";
+
+const App = (props:any) => {
+    return <Home />;
+}
+
+export default App;
