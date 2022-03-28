a8844fd4b27b0859dbc3993dc14390a9f05be94f
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-03-28 09:37:20 +0000

    remove body and add description

--- frontend/src/stories/AppInfo.stories.tsx
+++ frontend/src/stories/AppInfo.stories.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { AppInfo, AppInfoProps } from '../components/AppInfo'
 
 import { ComponentMeta } from '@storybook/react'
-import { AppIconProps } from '../components/AppIcon'
 import { retrieveListing } from '../actions/appLib'
 import { useState } from '@storybook/addons'
 
@@ -80,27 +79,6 @@ Updatable.args = {
     ...Primary.args,
     info: {
         ...Primary.args.info,
-        body: (
-            <>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting, remaining
-                    essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing software
-                    like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-                <h4>What's new</h4>
-                <ul>
-                    <li>Lorem lorem</li>
-                    <li>Lorem lorem</li>
-                </ul>
-            </>
-        ),
         isUpdatable: true,
     },
 }
