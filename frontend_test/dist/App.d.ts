332db5e1669362b411edf8d449b7c8a31ea56475
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-03-31 09:05:56 +0000

    removed front end test

--- frontend_test/dist/App.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const App: (props: any) => JSX.Element;
-export default App;
--- frontend_test/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { Home } from "dapp-store";
-
-const App = (props:any) => {
-    return <Home />;
-}
-
-export default App;
