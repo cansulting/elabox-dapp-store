cefc633b3a3d1c1ab9b78bfe935cb915c51a6e70
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-04-21 19:02:55 +0000

    change icon settings into number

--- frontend/src/components/AppDashboard.tsx
+++ frontend/src/components/AppDashboard.tsx
@@ -6,23 +6,21 @@ import useResize from "../hooks/useResize"
 export interface AppDashboardProps {
     apps: PackageInfo[],
     onClick?: (app : PackageInfo) => void,
-    iconWidth?: string
-    iconHeight?: string
+    iconWidth?: number
+    iconHeight?: number
     style?: object
 }
 
 export const AppDashboard = (props: AppDashboardProps): JSX.Element => {
     const parentDiv = useRef<HTMLDivElement>(null);    
-    const iconWidth = parseInt(props.iconWidth);
-    const iconHeight = parseInt(props.iconHeight);
     const {width : parentWidth} = useResize(parentDiv)
-    const iconWidthWithPadding = iconWidth + 40
+    const iconWidthWithPadding = props.iconWidth + 38
     const columnPerRow = Math.floor(parentWidth  / iconWidthWithPadding)
     if (props.apps === null) 
         return <></>
     return (
         <div style={props.style} ref={parentDiv}>
-            <Container style={{width:"100%"}}>
+            <Container>
                 <Row xs={columnPerRow}>
                     {props.apps.map((appInfo) => {
                         return (
@@ -30,8 +28,8 @@ export const AppDashboard = (props: AppDashboardProps): JSX.Element => {
                                 <AppIconCon 
                                     package={appInfo} 
                                     onClick={props.onClick}
-                                    width={iconWidth}
-                                    height={iconHeight}
+                                    width={props.iconWidth}
+                                    height={props.iconHeight}
                                     />
                             </Col>
                         )
