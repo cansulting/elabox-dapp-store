b218478070235703f916acc9a8f7cf4efadc89c6
Bon Nicolai Mercado <bonhokage06@gmmail.com>
2022-05-09 13:29:13 +0000

    create new error code

--- backend/service.go
+++ backend/service.go
@@ -54,7 +54,7 @@ func (instance *StoreService) rpc_retrievePackage(client protocol.ClientInterfac
 func (instance *StoreService) rpc_installPackage(client protocol.ClientInterface, action data.Action) string {
 	err := DownloadInstallApp(action.PackageId)
 	if err != nil {
-		broadcast.PublishError(action.PackageId, global.DOWNLOAD_ERROR, "Store server is down.")
+		broadcast.PublishError(action.PackageId, global.STORE_SERVER_ERROR, "Store server is down.")
 		return rpc.CreateResponse(rpc.INVALID_CODE, err.Error())
 	}
 	return rpc.CreateSuccessResponse("started")
