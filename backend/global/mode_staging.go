7e75c12d62e00f1a8d0e8fe5c9e6131196f590a3
Jhoemar Pagao <jhoemar_pagao@yahoo.com>
2022-06-07 12:06:53 +0000

    staging and debug server will be same

--- backend/global/mode_staging.go
+++ backend/global/mode_staging.go
@@ -3,6 +3,6 @@
 
 package global
 
-const STORE_HOST = "http://208.87.134.80:4001"
+const STORE_HOST = "http://208.87.134.80:4000"
 const ENV = "staging"
 const RETRIEVE_LISTING_DELAY = 5 // delay in retrieving store listing in sec
