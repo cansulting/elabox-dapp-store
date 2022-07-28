a628d359605e05f78eac21cbf31e2e3f59972904
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-07-28 05:30:28 +0000

    fix dashboard layout

--- frontend/src/components/AppDashboard.tsx
+++ frontend/src/components/AppDashboard.tsx
@@ -11,27 +11,32 @@ export interface AppDashboardProps {
     style?: object
     beta?:boolean 
 }
-
+let currentWidth = 0
 export const AppDashboard = (props: AppDashboardProps): JSX.Element => {
     const parentDiv = useRef<HTMLDivElement>(null);    
     const {width : parentWidth} = useResize(parentDiv)
-    const iconWidthWithPadding = props.iconWidth + 40
-    const columnPerRow = Math.floor(parentWidth  / iconWidthWithPadding)
-    const columnWidth = Math.floor(parentWidth / columnPerRow)
-    if (props.apps === null) 
+    const iconWidthWithPadding = props.iconWidth + 20
+    const columnPerRow = Math.round(parentWidth  / iconWidthWithPadding)
+    const columnWidth = Math.round(parentWidth / columnPerRow)
+    currentWidth = !isNaN(columnWidth) ? columnWidth : 0
+    if (props.apps === null){
         return <></>
-    return (
+
+    }    
+     return (
         <div style={props.style} ref={parentDiv}>
             <Row className="gx-2" xs={columnPerRow}>
                 {props.apps.map((appInfo) => {
                     return (
-                        <Col style={{width: columnWidth,maxWidth: columnWidth}} key={appInfo.id + "-dash"}>
+                        <Col 
+                        style={{minWidth:currentWidth,width: currentWidth,maxWidth: currentWidth}} 
+                        key={appInfo.id + "-dash"}>
                             <AppIconCon 
                                 package={appInfo} 
                                 onClick={props.onClick}
                                 width={props.iconWidth}
                                 height={props.iconHeight}
-                                />
+                            />                            
                         </Col>
                     )
                 })}
