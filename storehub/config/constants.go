ace7ccbdbb4bd336c9b9c985563925400a974650
Jhoemar <jhoemar_pagao@yahoo.com>
2022-09-20 06:33:03 +0000

    added storehub

---
+++ storehub/config/constants.go
@@ -0,0 +1,3 @@
+package config
+
+const LISTING_PATH = "data/stores.json"
---
+++ storehub/listing/storeinfo.go
@@ -0,0 +1,7 @@
+package listing
+type StoreInfo struct{
+	Id             string   `json:"id"`   // Store ID
+	Name           string   `json:"name"` // Store name
+	Description	   string `json:"description"` //Store Description
+	Icon           string `json:"icon"` //Store Icon
+}
