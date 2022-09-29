fab264195b6bc709c2ae60eece49f9cd708f7122
Bon Nicolai Mercado <bonhokage06@gmail.com>
2022-09-29 12:51:58 +0000

    remove dependency

--- client-store/frontend/src/App.tsx
+++ client-store/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route,useNavigate } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import useStoreHubStore from "./stores/storehub"
 import { StoreHubState } from "./interfaces/stores/storeHub"
 import DashboardController from "./controllers/Dashboard"
