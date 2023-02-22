6bfcacd8ee5d980c39769fde7630436b106da8e7
Jhoemar <jhoemar_pagao@yahoo.com>
2023-02-22 06:33:30 +0000

    updated configuration for storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,17 +1,27 @@
+const webpack = require('webpack');
+
 module.exports = {
-    "stories": [
-      "../src/**/*.stories.mdx",
-      "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
-    staticDir: '../assets',
-    "addons": [
-      "@storybook/addon-links",
-      "@storybook/addon-essentials",
-      "@storybook/addon-interactions",
-      "@storybook/preset-create-react-app",
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/preset-create-react-app',
     ],
-    "framework": "@storybook/react",
-    "core": {
-      "builder": "@storybook/builder-webpack5"
+    staticDirs: ['../src/assets'],
+    framework: '@storybook/react',
+    core: {
+        builder: 'webpack5',
+    },
+    webpackFinal: async (config, { configType }) => {
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+            })
+        )
+        return config;
     }
-  }
+}
